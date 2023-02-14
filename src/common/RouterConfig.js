7fef5fd8f3838dc2df57f82f750b47dd4a5fdf58
leejihui <dlwlgml20@gmail.com>
2023-02-15 00:10:25 +0900

    메인 페이지 진입시 소환사 검색 페이지로 이동

--- src/common/RouterConfig.js
+++ src/common/RouterConfig.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Main from '../pages/Main';
 
 const RouterConfig = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate replace to="/summoner" />} />
       <Route path="/summoner/*" element={<Main />} />
     </Routes>
   );
